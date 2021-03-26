80ad849e82c6a2ed583768b4ec810bd04fd49022
Alexandra <a.r.chumachenko@gmail.com>
2021-03-26 23:04:37 +0200

    Init express and model

---
+++ model.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "student.email@ukma.edu.ua": {
+        "fullName": "FirstName SecondName LastName",
+        "faculty": "facultyName",
+        "specialty": "specialtyName",
+        "yearOfStudy": 1,
+        "semesters": [
+            {
+                "semester": 1,
+                "courses": [
+                    {
+                        "name": "Course Name",
+                        "lector": "FirstName SecondName LastName",
+                        "lectorEmail": "lector@ukma.edu.ua",
+                        "mark": 100,
+                        "topics": [
+                            {
+                                "title": "Toopic name",
+                                "lab1": 5,
+                                "lab3": 5
+                            },
+                            {
+                                "title": "Topic name"
+                            }
+                        ]
+                    }
+                ]
+            },
+        ]
+    }
+};
