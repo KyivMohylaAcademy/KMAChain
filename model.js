adaf8274203ad5d15a60cff392c811d2d15a0e4f
Meredan <meredan.dn@gmail.com>
2021-03-26 21:02:49 +0200

    Add files via upload

---
+++ model.js
@@ -0,0 +1,94 @@
+module.exports = {
+    "email": {
+        "fullName": "Full Name",
+        "specialty": "Specialty",
+        "faculty": "Faculty",
+        "semesters": [
+            {
+                "semester": 1,
+                "subjects": [
+                    {
+                        "name": "ASD",
+                        "teacher": "Full Name",
+                        "mark": 91,
+                        "grade": "A",
+                        "topics": [
+                            {
+                                "topicName": "Topic one"
+                            },
+                            {
+                                "topicName": "Topic two"
+                            },
+                            {
+                                "topicName": "Topic three"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "OOP basics",
+                        "teacher": "Full Name",
+                        "mark": 61,
+                        "grade": "D",
+                        "topics": [
+                            {
+                                "topicName": "Topic one",
+                            },
+                            {
+                                "topicName": "Topic two",
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "semester": 2,
+                "subjects": [
+                    {
+                        "name": "Databases",
+                        "teacher": "Full Name",
+                        "mark": 63,
+                        "grade": "D",
+                        "topics": [
+                            {
+                                "topicName": "Topic one",
+                            },
+                            {
+                                "topicName": "Topic two",
+                            }
+                        ]
+                    },
+                    {
+                        "name": "Prolog basics",
+                        "teacher": "Full Name",
+                        "mark": 61,
+                        "grade": "D",
+                        "topics": [
+                            {
+                                "topicName": "Topic one",
+                            },
+                            {
+                                "topicName": "Topic two",
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "semester": 3,
+                "subjects": [
+                    {
+                        "name": "Client-server apps",
+                        "teacher": "Full Name",
+                        "mark": 81,
+                        "grade": "B",
+                        "topics": [
+                            {
+                                "topicName": "Topic one",
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+};
